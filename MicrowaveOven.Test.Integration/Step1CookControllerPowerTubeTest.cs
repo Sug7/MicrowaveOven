b857cb02a2f6f25ddbe9fb9ea98cdd972b189428
au536853 <au536853@uni.au.dk>
2019-11-28 12:18:28 +0100

    Step 1 and Step 2

--- MicrowaveOven.Test.Integration/Step1CookControllerPowerTubeTest.cs
+++ MicrowaveOven.Test.Integration/Step1CookControllerPowerTubeTest.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace MicrowaveOven.Test.Integration
 {
-    class Step1CookControllerPowerTubeTest
+    [TestFixture]
+    public class Step1CookControllerPowerTubeTest
     {
+        private ICookController _cookController;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IDisplay _display;
+        private IOutput _output;
+        private IUserInterface _userInterface;
+
+
+        public void SetUp()
+        {
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
+            _powerTube = new PowerTube(_output);
+            
+            // Laver en substitute instans for følgende fake modulerne
+            _timer = Substitute.For<ITimer>();
+            _display = Substitute.For<IDisplay>();
+            _output = Substitute.For<IOutput>();
+        }
+
+        [Test]
+        public void Stop_StartCooking_OffPower()
+        {
+            _cookController.StartCooking(50, 60);
+            _cookController.Stop();
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("Off")));
+        }
     }
 }
--- MicrowaveOven.Test.Integration/Step2CookControllerTest.cs
+++ MicrowaveOven.Test.Integration/Step2CookControllerTest.cs
@@ -2,11 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace MicrowaveOven.Test.Integration
 {
-    class Step2CookControllerTest
+    [TestFixture]
+    public class Step2CookControllerTest
     {
+        private ICookController _cookController;
+        private ITimer _timer; 
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private IOutput _output;
+        private IUserInterface _userInterface;
+
+
+        public void SetUp()
+        {
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
+            _powerTube = new PowerTube(_output);
+            _timer = new Timer();
+            // Laver en substitute instans for følgende fake modulerne
+            _display = Substitute.For<IDisplay>();
+            _output = Substitute.For<IOutput>();
+        }
+
+        // Test 1 Timer to CookController
+        [Test]
+        public void Start_StartCooking_Power()
+        {
+            int Watts = 60;
+            _cookController.StartCooking(Watts, 60);
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains(Watts.ToString())));
+        }
+
+        [Test]
+        public void Start_StartCooking_StopOff()
+        {
+            _cookController.StartCooking(60, 60);
+            Thread.Sleep(2000);
+            _cookController.Stop();
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("Off")));
+        }
+
+        [Test]
+        public void Start_StartCooking_ExpireOff()
+        {
+            int time = 2;
+            _cookController.StartCooking(60, time);
+            Thread.Sleep(3000);
+            _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("Off")));
+        }
+
+        // Test 2 CookController to Timer
+        [TestCase(610, 610)]
+        [TestCase(0, 0)]
+        [TestCase(100, 100)]
+        public void Start_StartCooking_RemainingTime(int time, int remainingTime)
+        {
+            _cookController.StartCooking(50, time);
+            Assert.That(_timer.TimeRemaining, Is.EqualTo(remainingTime));
+        }
+
+
+
+
     }
 }
